abeadec0a34f167affaa5b8a6b93a7f5b7db2be8
kamileregina <39653747+kamileregina@users.noreply.github.com>
2018-06-16 15:18:49 -0300

    Add files via upload

---
+++ Calculadora/ADDITION.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+import selenium.webdriver.support.ui as UI
+
+
+driver = webdriver.Chrome()
+# Open the desired web page
+driver.get('http://juliemr.github.io/protractor-demo/')
+
+
+# Type in numbers for operation
+first_number = driver.find_element_by_css_selector('[ng-model="first"]')
+first_number.send_keys('51')
+second_number = driver.find_element_by_css_selector('[ng-model="second"]')
+second_number.send_keys('49')
+
+#erformed
+operation_add = UI.Select(driver.find_element_by_css_selector('[ng-model=operator]')).select_by_value("ADDITION")
+
+# Click on the button to perform operation
+result_button = driver.find_element_by_class_name('btn')
+result_button.click()
+
+# Await operation to be performed
+time.sleep(4)
+result = driver.find_element_by_class_name('ng-binding')
+
+# Assert operation result and finish browser instance
+assert result.text == '100'
+driver.quit()
